1f6e5e73b0301321b5659ae1dc997ea4a873997c
Java <sudiptomandolsm@gmail.com>
2025-01-30 23:08:57 +0600

    inetial commit for this project

---
+++ src/Display.java
@@ -0,0 +1,56 @@
+public class Display {
+    public static void main(String[] args ){
+            Member name1=new Member();
+
+
+            System.out.println(" ");
+            Member name2=new Member();
+
+            name2.name="DULAL MANDAL";
+            name2.address="Tentulbari";
+            name2.id=1;
+            name2.age=55;
+
+            System.out.println("name:"+name2.name);
+            System.out.println("address:"+name2.address);
+            System.out.println("id:"+name2.id);
+            System.out.println("age:"+name2.age);
+
+            System.out.println(" ");
+            Member name3=new Member();
+            name3.name="SAGORIKA MANDAL";
+            name3.address="Tentulbari";
+            name3.id=2;
+            name3.age=43;
+
+            System.out.println("name:"+name3.name);
+            System.out.println("address:"+name3.address);
+            System.out.println("id:"+name3.id);
+            System.out.println("age:"+name3.age);
+
+            System.out.println(" ");
+            name1.name="SUDIPTO MANDAL";
+            name1.address="mirpur";
+            name1.id=3;
+            name1.age=22;
+            System.out.println("name:"+name1.name);
+            System.out.println("address:"+name1.address);
+            System.out.println("id:"+name1.id);
+            System.out.println("age:"+name1.age);
+
+            System.out.println(" ");
+            Member name4=new Member();
+            name4.name="HORI MANDAL";
+            name4.address="Tentulbari";
+            name4.id=4;
+            name4.age=15;
+
+            System.out.println("name:"+name4.name);
+            System.out.println("address:"+name4.address);
+            System.out.println("id:"+name4.id);
+            System.out.println("age:"+name4.age);
+
+
+        }
+    }
+
---
+++ src/Member.java
@@ -0,0 +1,8 @@
+public class Member {
+
+        String name,address;
+        int id;
+        int age;
+
+
+}

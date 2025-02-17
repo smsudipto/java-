e90ab7c7831bdca2354bfcb7dea4c69f53d4d0fe
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-02-18 00:30:16 +0600

    Add files via upload

---
+++ Fibonacci_Sequence.java
@@ -0,0 +1,28 @@
+package Fibonacci;
+
+import java.util.Scanner;
+
+public class Fibonacci_Sequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int n = sc.nextInt();
+
+       int first=0;
+       int second=1;
+        System.out.print("Fibonacci Series: " + first + ", " + second);
+       for(int i=3;i<=n;i++)
+       {
+           int fibo=first+second;
+           System.out.print(", " + fibo);
+           first=second;
+           second=fibo;
+
+       }
+        System.out.println(); // To move to the next line after the series
+        sc.close();
+
+
+    }
+
+}

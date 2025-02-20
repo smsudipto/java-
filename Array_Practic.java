5f2abdccfde796383fba892f79a5573549b7c46f
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-02-20 14:04:18 +0600

    Add files via upload

---
+++ Array_Practic.java
@@ -0,0 +1,30 @@
+package Array_demo;
+
+public class Array_Practic {
+    public static void main(String[] args) {
+        int[] array =new int[10],  array2=new int[20];
+        array[0]=10;
+        array[1]=20;
+        array[2]=30;
+        array[3]=40;
+        array[4]=50;
+        array[5]=60;
+        array[6]=70;
+        array[7]=80;
+        array[8]=90;
+        array[9]=100;
+            System.out.println("Array is:"+array[0]);
+            System.out.println("Array is:"+array[1]);
+            System.out.println("Array is:"+array[2]);
+            System.out.println("Array is:"+array[3]);
+            System.out.println("Array is:"+array[4]);
+            System.out.println("Array is:"+array[5]);
+            System.out.println("Array is:"+array[6]);
+            System.out.println("Array is:"+array[7]);
+            System.out.println("Array is:"+array[8]);
+            System.out.println("Array is:"+array[9]);
+
+        array2[0]=1000;
+        System.out.println("Array2 is:"+array2[0]);
+    }
+}

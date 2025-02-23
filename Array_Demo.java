e5f42be5431726629521b5334597168acc7f1dea
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-02-23 09:54:24 +0600

    Add files via upload

---
+++ Array_Demo.java
@@ -0,0 +1,39 @@
+package Array_demo;
+
+public class Array_Demo {
+    public static void main(String[] args) {
+                int[] array =new int[10];
+                array[0]=10;
+                array[1]=20;
+                array[2]=30;
+                array[3]=40;
+                array[4]=50;
+                array[5]=60;
+                array[6]=70;
+                array[7]=80;
+                array[8]=90;
+                array[9]=100;
+                System.out.println("Array is:"+array[0]);
+                System.out.println("Array is:"+array[1]);
+                System.out.println("Array is:"+array[2]);
+                System.out.println("Array is:"+array[3]);
+                System.out.println("Array is:"+array[4]);
+                System.out.println("Array is:"+array[5]);
+                System.out.println("Array is:"+array[6]);
+                System.out.println("Array is:"+array[7]);
+                System.out.println("Array is:"+array[8]);
+                System.out.println("Array is:"+array[9]);
+
+              int len=array.length;
+              System.out.println("Length of array is:"+len);
+              for(int i=0;i<len;i++){
+                  System.out.println(array[i]);
+                  System.out.println("Length of array3 is:" +array[3]);
+
+
+              }
+            }
+        }
+
+
+

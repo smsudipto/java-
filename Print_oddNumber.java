3c30750723ca4e8c905014c1676236dddbc900e1
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-03-01 01:27:44 +0600

    Add files via upload

---
+++ Print_oddNumber.java
@@ -0,0 +1,23 @@
+package Demo_forLoop;
+
+public class Print_oddNumber {
+    public static void main(String[] args)
+    {
+        for(int i=0;i<=10;i++)
+        {
+            //if(i%2==0)
+          //  {
+          //      continue;
+            // }
+
+//            System.out.println(i+" odd neumer.");
+
+            if(i==6)
+            {
+                break;
+            }
+            System.out.println(i+ " is a number.");
+        }
+    }
+
+}

34c6df319b5e3deea1fd00d73394ce303509750f
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-03-05 23:18:59 +0600

    Add files via upload

---
+++ Descending_order.java
@@ -0,0 +1,19 @@
+package Demo_Descending_order;
+
+public class Descending_order {
+    public static void main(String[] args)
+    {
+int n=10;
+        for(int a=n;a>=1;a--)
+        {
+            System.out.println(a);
+            if(a==5)
+            {
+                break;
+            }
+
+        }
+    }
+
+
+}

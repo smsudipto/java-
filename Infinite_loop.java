ad71f8ee31790ba1ee76edb2c219957ac1e731c6
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-03-21 19:11:16 +0600

    Add files via upload

---
+++ Infinite_loop.java
@@ -0,0 +1,16 @@
+package Demo_Infinite_loop;
+
+public class Infinite_loop {
+
+        public static void main(String[] args)
+        {
+            for (;;)
+            {
+                System.out.println("I love you babu");
+            }
+        }
+
+
+    }
+
+

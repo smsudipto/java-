9f34f07882260569f04612eb6983375740e0e455
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-04-08 09:01:27 +0600

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,31 @@
+package Factorial;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[]args){
+
+
+            //int n=0;
+            int sum=1;
+//System.out.println("Inter your n:");
+//Scanner input=new Scanner(System.in);
+//n=input.nextInt();
+
+            System.out.println("The value is:");
+            for(int i=1;10>=i;i++)
+            {
+
+
+                sum=5*i;
+
+                System.out.println(+5+"X"+i+"="+sum);
+
+            }
+
+
+
+
+        }
+    }
+

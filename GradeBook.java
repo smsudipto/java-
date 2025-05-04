af8bfd609eb7a65a37a9550310440cdfe06bc7e1
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-05-04 09:47:02 +0600

    Add files via upload

---
+++ GradeBook.java
@@ -0,0 +1,13 @@
+package GradeBookDemo;
+
+public class GradeBook {
+    public void displayMessage(){
+        System.out.println("Welcome to the Grade Book!");
+    }
+
+    public static void main(String[] args) {
+        GradeBook myGradeBook=new GradeBook();
+        myGradeBook. displayMessage();
+    }
+
+}

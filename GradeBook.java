37dbc8da56a7c75bd4ad6cbbc8b817de7f8f1535
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-06-01 19:10:54 +0600

    Add files via upload

--- GradeBook.java
+++ GradeBook.java
@@ -1,13 +1,21 @@
-package GradeBookDemo;
+import java.util.Scanner;
 
 public class GradeBook {
-    public void displayMessage(){
-        System.out.println("Welcome to the Grade Book!");
-    }
 
+    public void displayMessage(String courseName) {
+        System.out.printf("Welcome to the grade book for %s!", courseName);
+    }
+}
+
+
+class GradeBookTest {
     public static void main(String[] args) {
-        GradeBook myGradeBook=new GradeBook();
-        myGradeBook. displayMessage();
-    }
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the course name:");
+        String nameOfCourse = input.nextLine();
 
+        GradeBook myGradeBook = new GradeBook();
+        System.out.println();
+        myGradeBook.displayMessage(nameOfCourse);
+    }
 }

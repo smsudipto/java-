8833f5404dc747c1b9f35a1ef540445e24e8ddd8
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-06-26 23:35:24 +0600

    Add files via upload

--- Geeks.java
+++ Geeks.java
@@ -1,27 +1,21 @@
-package LinkedHashMap;
-
+package HashMap;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
-
 public class Geeks {
+    public static void main(String[] args) {
+        //creating a map using HashMap;
+        Map<String,Integer> M=new HashMap<>();
+        //Adding key value paire to map;
+        M.put("Sudipto",1);
+        M.put("Shojib",2);
+        M.put("Raihan",3);
+        System.out.println(" Map implement is:"+M );
 
-        public static void main(String[] args) {
-            //creating a map using HashMap;
-            Map<String,Integer> M=new LinkedHashMap<>();
-            //Adding key value paire to map;
-            M.put("Sudipto",1);
-            M.put("Shojib",2);
-            M.put("Raihan",3);
-            System.out.println(" Map implement is:"+M );
-
-            for(Map.Entry<String,Integer>me:M.entrySet())
-            {
-                System.out.print(me.getKey()+":");
-                System.out.println(me.getValue());
-            }
-
+        for(Map.Entry<String,Integer>me :M.entrySet())
+        {
+            System.out.print(me.getKey()+":" );
+            System.out.println(me.getValue());
         }
+
     }
-
-
+}

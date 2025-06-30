f311ecae6748069becca93ebafca72e1cc6afc86
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-06-30 22:16:05 +0600

    Add files via upload

---
+++ RemovingElement.java
@@ -0,0 +1,24 @@
+package ArrayList;
+import java.util.*;
+public class RemovingElement {
+        public static void main(String[] args) {
+            List<Integer> li=new ArrayList<>();
+            li.add(1);
+            li.add(2);
+            li.add(3);
+            li.add(4);
+            li.add(5);
+            li.add(6);
+            System.out.println(li);
+
+            li.remove(2);
+            System.out.println("updeting element is:"+li);
+        }
+    }
+
+
+
+
+
+
+

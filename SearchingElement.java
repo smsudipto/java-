86d2ee5fb3f4b6752eb1682489b24be7a076d151
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2025-06-30 22:16:29 +0600

    Add files via upload

---
+++ SearchingElement.java
@@ -0,0 +1,27 @@
+package ArrayList;
+import java.util.*;
+public class SearchingElement {
+    public static void main(String[] args) {
+                List<Integer> li=new ArrayList<>();
+                li.add(2);
+                li.add(3);
+                li.add(1);
+                li.add(1);
+                li.add(2);
+                li.add(2);
+                System.out.print(li);
+
+                int i=li.indexOf(1);
+        System.out.println("Index is:"+i);
+
+        int j=li.lastIndexOf(2);
+        System.out.println("last index is:"+j);
+
+
+            }
+        }
+
+
+
+
+
